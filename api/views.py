4ec60a94a27992d6c7c812b48638e0f9e1ab399a
Arjit Shukla <arjitshukla286@gmail.com>
2026-04-20 21:55:48 +0530

    feature: JWT authentication implementation

--- api/views.py
+++ api/views.py
@@ -4,32 +4,16 @@ from django.db.models.query import QuerySet
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Doctor, Appointment
 from .serializers import DoctorSerializer, AppointmentSerializer
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
 
 class DoctorViewSet(ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Doctor.objects.all().order_by("name")
     serializer_class = DoctorSerializer
 
-
-# class AppointmentViewSet(ModelViewSet):
-#     queryset = Appointment.objects.select_related('doctor')
-#     serializer_class = AppointmentSerializer
-
-#     def get_queryset(self):
-#         queryset = super().get_queryset().order_by("date", "time_slot")
-
-#         doctor_id = self.request.query_params.get('doctor_id')
-#         date = self.request.query_params.get('date')
-
-#         if doctor_id:
-#             queryset = queryset.filter(doctor_id=doctor_id)
-
-#         if date:
-#             queryset = queryset.filter(date=date)
-
-#         return queryset
-
 class AppointmentViewSet(ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Appointment.objects.select_related('doctor').order_by("date", "time_slot")
     serializer_class = AppointmentSerializer
     filter_backends = [DjangoFilterBackend]
--- myproject/settings.py
+++ myproject/settings.py
@@ -9,11 +9,10 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
 import os
+from datetime import timedelta
 from pathlib import Path
 from dotenv import load_dotenv
-
 load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +37,20 @@ INSTALLED_APPS = [
     'django_filters',
     'api',
 ]
+# Django REST Framework configuration
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend"
+    ],
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -107,7 +120,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
@@ -124,9 +136,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
-REST_FRAMEWORK = {
-    "DEFAULT_FILTER_BACKENDS": [
-        "django_filters.rest_framework.DjangoFilterBackend"
-    ]
-}
